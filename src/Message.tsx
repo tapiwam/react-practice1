ccfa16c325cd11ba62e72c9b93ce5974906336ee
Tapiwa Maruni <tapiwamaruni@Tapiwas-Mac-mini.local>
2023-04-22 17:35:06 -0500

    Sample listgroup component

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,11 +1,8 @@
-
 function Message() {
-    // JSX: Javascript
-    const name = 'Tapiwa';
-    if(name)
-        return <h1>Hello {name}</h1>
-    else
-        return <h1>Hellow World</h1>
+  // JSX: Javascript
+  const name = "T";
+  if (name) return <h1>Hello {name}</h1>;
+  else return <h1>Hellow World</h1>;
 }
 
 export default Message;
