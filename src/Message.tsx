e05fd3de6bd56ae99c7af4ed2554a906304bb3de
Tapiwa Maruni <tapiwamaruni@Tapiwas-Mac-mini.local>
2023-04-22 16:25:19 -0500

    Initial commit

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+
+function Message() {
+    // JSX: Javascript
+    const name = 'Tapiwa';
+    if(name)
+        return <h1>Hello {name}</h1>
+    else
+        return <h1>Hellow World</h1>
+}
+
+export default Message;
