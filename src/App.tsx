6fd86aeee079f830bc6ed8bd733aaa68bf67abe4
Tapiwa Maruni <tapiwamaruni@Tapiwas-Mac-mini.local>
2023-04-23 15:08:38 -0500

    Using react icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,12 @@ import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
+import { BsFillCalendarWeekFill } from "react-icons/bs";
 
 function App() {
-  const [alertVisible, setAlertVisible] = useState(false);
-  let items = ["Dallas", "Houston", "Tampa"];
-
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>
-      )}
-
-      <Button color="primary" onClick={() => setAlertVisible(true)}>
-        Click Me
-      </Button>
-
-      <hr />
-
-      <ListGroup
-        heading="Cities"
-        items={items}
-        onSelectItem={() => console.log("clicked")}
-      ></ListGroup>
+      <BsFillCalendarWeekFill color="red" size={40}></BsFillCalendarWeekFill>
     </div>
   );
 }
