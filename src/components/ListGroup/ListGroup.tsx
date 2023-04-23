4f2fb8d2243f4bca3478b877ff98c93e5fdbf0ed
Tapiwa Maruni <tapiwamaruni@Tapiwas-Mac-mini.local>
2023-04-23 14:59:05 -0500

    Stlying with inline CSS

--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,21 +1,4 @@
 import { useState } from "react";
-import styled from "styled-components";
-
-const List = styled.ul`
-  .list-group {
-    list-style: none;
-    padding: 0;
-  }
-`;
-
-interface ListItemProps {
-  active: boolean;
-}
-
-const ListItem = styled.li<ListItemProps>`
-  padding: 5px 0;
-  background: ${(props) => (props.active ? "blue" : null)};
-`;
 
 interface Props {
   items: string[];
@@ -30,10 +13,9 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
     <>
       <h1>{heading}</h1>
       {items.length === 0 && <p>No items found</p>}
-      <List className="list-group">
+      <ul className="list-group" style={{ backgroundColor: "yellow" }}>
         {items.map((item, index) => (
-          <ListItem
-            active={index === selectedIndex}
+          <li
             key={item}
             onClick={() => {
               setSelectedIndex(index);
@@ -41,9 +23,9 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
             }}
           >
             {item}
-          </ListItem>
+          </li>
         ))}
-      </List>
+      </ul>
     </>
   );
 }
