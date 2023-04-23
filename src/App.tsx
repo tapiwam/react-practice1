770deef65599613f4c88f337106d39e75d30745e
Tapiwa Maruni <tapiwamaruni@Tapiwas-Mac-mini.local>
2023-04-23 14:25:27 -0500

    Styling with CSS modules

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
+import "./App.css";
 
 function App() {
   const [alertVisible, setAlertVisible] = useState(false);
