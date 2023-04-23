83625b4404a23e830ef9a0005472b5b675c35c64
Tapiwa Maruni <tapiwamaruni@Tapiwas-Mac-mini.local>
2023-04-23 10:34:52 -0500

    Passing children to component using ReactNode

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
