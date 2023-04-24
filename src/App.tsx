b06e3c07eee626dfac794653aa1e823f40a3a380
Tapiwa Maruni <tapiwamaruni@Tapiwas-Mac-mini.local>
2023-04-24 06:38:55 -0500

    Updated nested objects in state

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,26 @@ function App() {
   const [person, setPerson] = useState({
     firstName: "T",
     lastName: "M",
+    address: {
+      city: "Dallas",
+      zipCode: 75126,
+    },
   });
 
   const handleClick = () => {
-    setPerson({ ...person, firstName: "Tapiwa" });
+    // When spreading remember "it is shallow". Need to traverse nested objects
+    setPerson({
+      ...person,
+      firstName: "Tapiwa",
+      address: { ...person.address, zipCode: 75254 },
+    });
   };
 
   return (
     <div>
-      {person.firstName} {person.lastName}
+      {person.firstName} {person.lastName} <br />
+      {person.address.city} {person.address.zipCode}
+      <br />
       <button onClick={handleClick}>Click Me</button>
     </div>
   );
