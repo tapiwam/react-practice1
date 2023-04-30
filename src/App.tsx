89cc054a53222525989168140084f8b902ccf03f
Tapiwa Maruni <tapiwa.maruni@gmail.com>
2023-04-30 00:45:01 -0500

    Add basic react form with basic input elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
 import { useState } from "react";
-import produce from "immer";
-import ListGroup from "./components/ListGroup";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
+import Form from "./components/Form";
 
 function App() {
-  const [cart, setCart] = useState(["Product1", "Product2"]);
-
-  const handleClick = () => {
-    // setting arrays in state also need new arrays
-
-    // Update a value -> Map to find then spread the inner object with updates
-    // setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
-    setCart([...cart, "Produce3"]);
-  };
-
-  const handleClearCart = () => {
-    setCart([]);
-  };
-
   return (
     <div>
-      <button onClick={handleClick}>Add To Cart</button>
-      <br />
-
-      <NavBar cartCount={cart.length}></NavBar>
-      <Cart cartItems={cart} onClear={handleClearCart}></Cart>
+      <Form></Form>
     </div>
   );
 }
